b1d2ff92b4198c6055ba7391a5c09950ac9180d9
David Bergqvist <David.bergqvist@gmail.com>
2024-04-25 15:54:05 +0000

    debug test

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2l=)onf@14aye3a9o%^_&r)366orqrc5q4=lbjfrrr=qwv8o3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-davidb3rgqv-djangoproje-ia8s81b1d1u.ws-eu110.gitpod.io','.herokuapp.com']
 
